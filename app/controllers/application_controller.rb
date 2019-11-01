c917efe0fd33d1ddcfa19423b068cb09c903f2c5
001wwang <williamwang@uchicago.edu>
2019-11-01 21:25:43 +0000

    Add URL matches for user and photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,49 @@
 class ApplicationController < ActionController::Base
+
+  def all_users
+    render({:json => User.all})
+  end
+
+  def user
+    render({:json => User.where({username: params['username']}).at(0)})
+  end
+
+  def user_photos
+    u = User.where({username: params['username']}).at(0)
+    render({:json => u.own_photos})
+  end
+
+  def user_liked_photos
+    u = User.where({username: params['username']}).at(0)
+    render({:json => u.liked_photos})
+  end
+
+  def user_feed
+    u = User.where({username: params['username']}).at(0)
+    render({:json => u.feed})
+  end
+
+  def user_discover
+    u = User.where({username: params['username']}).at(0)
+    render({:json => u.discover})
+  end
+
+  # Photo
+  def photo
+    render({:json => Photo.where({id: params['photo_id']}).at(0)})
+  end
+
+  def photo_likes
+    photo = Photo.where({id: params['photo_id']}).at(0)
+    render({:json => photo.likes})
+  end
+
+  def photo_fans
+    photo = Photo.where({id: params['photo_id']}).at(0)
+    render({:json => photo.fans})
+  end
+
+  def insert_like_record
+    render({:json => params})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+  # User
+  match("/users", {controller: "application", action: "all_users", via: "get"})
+  match("/users/:username", {controller: "application", action: "user", via: "get"})
+  match("/users/:username/own_photos", {controller: "application", action: "user_photos", via: "get"})
+  match("/users/:username/liked_photos", {controller: "application", action: "user_liked_photos", via: "get"})
+  match("/users/:username/feed", {controller: "application", action: "user_feed", via: "get"})
+  match("/users/:username/discover", {controller: "application", action: "user_discover", via: "get"})
 
+  # Photo
+  match("/photos/:photo_id", {controller: "application", action: "photo", via: "get"})
+  match("/photos/:photo_id/likes", {controller: "application", action: "photo_likes", via: "get"})
+  match("/photos/:photo_id/fans", {controller: "application", action: "photo_fans", via: "get"})
 
+  # Insert like record
+  match("/insert_like_record", {controller: "application", action: "insert_like_record", via: "get"})
   # =================================================
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
